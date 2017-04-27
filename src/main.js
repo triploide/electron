07c528a2c9bbfa00c5b4c630b9f20ae0141b94ce
Maxi Yañez <aeroventila@gmail.com>
2017-04-27 09:01:48 -0300

    pendiente

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ const electron = require('electron');
 const {app, BrowserWindow, Menu} = electron;
 
 function createMainMenu () {
-	let template; //json
-	Menu.setApplicationMenu(Menu.buildFromTemplete(template));
+    let template; //json
+    Menu.setApplicationMenu(Menu.buildFromTemplete(template));
 }
 
 app.on('ready', () => {
-	let win = new BrowserWindow({width: 800, height: 600});
-	win.loadURL(`file://${__dirname}/index.html`);
+    let win = new BrowserWindow({width: 800, height: 600});
+    win.loadURL(`file://${__dirname}/couch.html`);
+    win.webContents.openDevTools();
 });
+
+app.on('window-all-closed', () => {
+    app.quit()
+})
